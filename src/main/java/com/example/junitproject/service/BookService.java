d45899a208d142ad163c47d3979023f95fb5a7f2
Hyuntae <bht9011@gmail.com>
2022-09-19 00:00:07 +0900

    completed editBook on BookService

--- src/main/java/com/example/junitproject/service/BookService.java
+++ src/main/java/com/example/junitproject/service/BookService.java
@@ -35,7 +35,7 @@ public class BookService {
     // 3. 책한권보기
     public BookResponseDto getBook(Long id) {
         Optional<Book> bookOP = bookRepository.findById(id);
-        if(bookOP.isPresent()) {
+        if (bookOP.isPresent()) {
             return new BookResponseDto().toDto(bookOP.get());
         } else {
             throw new RuntimeException("해당 아이디를 찾을 수 없습니다.");
@@ -43,7 +43,20 @@ public class BookService {
     }
 
     // 4. 책삭제
-
+    @Transactional(rollbackFor = RuntimeException.class)
+    public void deleteBook(Long id) { // 4
+        bookRepository.deleteById(id); // 1, 2, 3
+    }
 
     // 5. 책수정
+    @Transactional(rollbackFor = RuntimeException.class)
+    public void editBook(Long id, BookSaveRequestDto dto) { // id, title, author
+        Optional<Book> bookOP = bookRepository.findById(id);// 1, 2, 3
+        if(bookOP.isPresent()) {
+            Book bookPS = bookOP.get();
+            bookPS.update(dto.getTitle(), dto.getAuthor());
+        } else {
+            throw new RuntimeException("해당 아이디를 찾을 수 없습니다.");
+        }
+    }
 }
