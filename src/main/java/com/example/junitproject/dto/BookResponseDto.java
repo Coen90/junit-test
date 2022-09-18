cbce86c8a5fa185f77740f5dd79fea2e580cf987
hyuntae <bht9011@gmail.com>
2022-09-18 19:06:43 +0900

    completed addBook on BookService

--- src/main/java/com/example/junitproject/dto/BookResponseDto.java
+++ src/main/java/com/example/junitproject/dto/BookResponseDto.java
@@ -1,4 +1,21 @@
 package com.example.junitproject.dto;
 
+import com.example.junitproject.domain.Book;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@NoArgsConstructor
 public class BookResponseDto {
+
+    private Long id;
+    private String title;
+    private String author;
+
+    public BookResponseDto toDto(Book bookPS) {
+        this.id = bookPS.getId();
+        this.title = bookPS.getTitle();
+        this.author = bookPS.getAuthor();
+        return this;
+    }
 }
--- src/main/java/com/example/junitproject/service/BookService.java
+++ src/main/java/com/example/junitproject/service/BookService.java
@@ -1,7 +1,34 @@
 package com.example.junitproject.service;
 
+import com.example.junitproject.domain.Book;
+import com.example.junitproject.domain.BookRepository;
+import com.example.junitproject.dto.BookResponseDto;
+import com.example.junitproject.dto.BookSaveRequestDto;
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 @Service
+@RequiredArgsConstructor
 public class BookService {
+
+    private final BookRepository bookRepository;
+
+    // 1. 책 등록
+    @Transactional(rollbackFor = RuntimeException.class)
+    public BookResponseDto addBook(BookSaveRequestDto dto) {
+        Book bookPS = bookRepository.save(dto.toEntity());
+        return new BookResponseDto().toDto(bookPS);
+    }
+
+    // 2. 책 목록보기
+
+
+    // 3. 책한건보기
+
+
+    // 4. 책삭제
+
+
+    // 5. 책수정
 }
