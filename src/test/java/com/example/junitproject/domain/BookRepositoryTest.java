dcc416618ee91e4208029f7e51e406d719a1539b
hyuntae <bht9011@gmail.com>
2022-09-18 17:11:17 +0900

    completed book save test

--- src/test/java/com/example/junitproject/domain/BookRepositoryTest.java
+++ src/test/java/com/example/junitproject/domain/BookRepositoryTest.java
@@ -1,8 +1,11 @@
 package com.example.junitproject.domain;
 
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+
+import static org.junit.jupiter.api.Assertions.*;
 
 @DataJpaTest // DB와 관련된 컴포넌트만 메모리에 로딩
 class BookRepositoryTest {
@@ -13,7 +16,20 @@ class BookRepositoryTest {
     // 1. 책 등록
     @Test
     void 책등록_test() {
-        System.out.println("책 등록_test 실행");
+        // given (데이터 준비)
+        String title = "junit5";
+        String author = "코엔";
+        Book book = Book.builder()
+                .title(title)
+                .author(author)
+                .build();
+
+        // when (테스트 실행)
+        Book bookPS = bookRepository.save(book);
+
+        // then (검증)
+        assertEquals(title, bookPS.getTitle());
+        assertEquals(author, bookPS.getAuthor());
 
     }
 
