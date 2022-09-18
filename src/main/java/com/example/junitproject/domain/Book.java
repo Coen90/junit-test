517ace993fcd399dcea33b1d0f70cf50f3a893d3
hyuntae <bht9011@gmail.com>
2022-09-18 16:49:52 +0900

    created entity

--- src/main/java/com/example/junitproject/domain/Book.java
+++ src/main/java/com/example/junitproject/domain/Book.java
@@ -1,6 +1,31 @@
 package com.example.junitproject.domain;
 
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import org.springframework.context.annotation.Primary;
+
+import javax.persistence.*;
+
+@Entity
+@Getter
+@NoArgsConstructor
 public class Book {
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
 
+    @Column(length = 50, nullable = false)
+    private String title;
+
+    @Column(length = 20, nullable = false)
+    private String author;
+
+    @Builder
+    public Book(Long id, String title, String author) {
+        this.id = id;
+        this.title = title;
+        this.author = author;
+    }
 }
